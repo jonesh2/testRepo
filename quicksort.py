9ae28f7e704b987308a39e135f3fcd269764e169
jonesh2 <jonesh2@carleton.edu>
2016-11-10 22:52:45 -0600

    Base quicksort

---
+++ quicksort.py
@@ -0,0 +1,38 @@
+def quicksort(myList, start, end):
+    if start < end:
+        # partition the list
+        pivot = partition(myList, start, end)
+        # sort both halves
+        quicksort(myList, start, pivot-1)
+        quicksort(myList, pivot+1, end)
+
+
+def partition(myList, start, end):
+    pivot = myList[start]
+    left = start+1
+    right = end
+    done = False
+    while not done:
+        while left <= right and myList[left] <= pivot:
+            left = left + 1
+        while myList[right] >= pivot and right >=left:
+            right = right -1
+        if right < left:
+            done= True
+        else:
+            # swap places
+            temp=myList[left]
+            myList[left]=myList[right]
+            myList[right]=temp
+    # swap start with myList[right]
+    temp=myList[start]
+    myList[start]=myList[right]
+    myList[right]=temp
+    return right
+
+def main():
+    myList = [3, 2, 9, 0, 7, 3, 5, 12]
+    quicksort(myList, 0, len(myList)-1)
+    print myList
+
+main()
