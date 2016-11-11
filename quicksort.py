36e4f035cfda88f193342e50a9f4dba14d8a572f
jonesh2 <jonesh2@carleton.edu>
2016-11-10 23:12:24 -0600

    try again

--- quicksort.py
+++ quicksort.py
@@ -1,12 +1,14 @@
-def quicksort(myList, start, end):
-    if start < end:
-        # partition the list
-        pivot = partition(myList, start, end, start+1, end)
-        # sort both halves
-        quicksort(myList, start, pivot-1)
-        quicksort(myList, pivot+1, end)
+def switch(myList, one, two):
+    temp=myList[one]
+    myList[one]=myList[two]
+    myList[two]=temp
 
-def partition(myList, start, end, left, right):
+def sortMyList(myList):
+    newList = myList.copy()
+    quicksort(newList, 0, len(newList))
+    return newList
+
+def divide(myList, start, end):
     pivot = myList[start]
     left = start+1
     right = end
@@ -20,19 +22,22 @@ def partition(myList, start, end, left, right):
             done= True
         else:
             # swap places
-            swap(myList, left, right)
+            switch(myList, left, right)
     # swap start with myList[right]
-    swap(myList, start, right)
+    switch(myList, start, right)
     return right
 
-def swap(myList, one, two):
-    myList[one]=myList[two]
-    myList[two]=myList[one]
-
+def quicksort(myList, start, end):
+    if start < end:
+        # partition the list
+        pivot = divide(myList, start, end)
+        # sort both halves
+        conquer(myList, start, pivot-1)
+        conquer(myList, pivot+1, end)
 
 def main():
     myList = [3, 2, 9, 0, 7, 3, 5, 12]
-    quicksort(myList, 0, len(myList)-1)
+    myList = quicksort(myList, 0, len(myList)-1)
     print myList
 
 main()
