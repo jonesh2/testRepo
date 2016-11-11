3e68a45829b40e3be24f7af640eb23d563219093
jonesh2 <jonesh2@carleton.edu>
2016-11-11 07:45:04 -0600

    Original quicksort implemented

--- quicksort.py
+++ quicksort.py
@@ -1,12 +1,12 @@
 def quicksort(myList, start, end):
     if start < end:
         # partition the list
-        pivot = partition(myList, start, end, start+1, end)
+        pivot = partition(myList, start, end)
         # sort both halves
         quicksort(myList, start, pivot-1)
         quicksort(myList, pivot+1, end)
 
-def partition(myList, start, end, left, right):
+def partition(myList, start, end):
     pivot = myList[start]
     left = start+1
     right = end
@@ -20,15 +20,14 @@ def partition(myList, start, end, left, right):
             done= True
         else:
             # swap places
-            swap(myList, left, right)
+            temp=myList[left]
+            myList[left]=myList[right]
+            myList[right]=temp
     # swap start with myList[right]
-    swap(myList, start, right)
+    temp=myList[start]
+    myList[start]=myList[right]
+    myList[right]=temp
     return right
-
-def swap(myList, one, two):
-    myList[one]=myList[two]
-    myList[two]=myList[one]
-
 
 def main():
     myList = [3, 2, 9, 0, 7, 3, 5, 12]
