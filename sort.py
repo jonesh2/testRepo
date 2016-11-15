b3a889c87de6895544fa7e619c073f4b3e908696
jonesh2 <jonesh2@carleton.edu>
2016-11-15 12:11:13 -0600

    started implementing quicksort.py

--- sort.py
+++ sort.py
@@ -1,15 +1,36 @@
+def partition(myList, start, end):
+    pivot = myList[start]
+    left = start+1
+    right = end
+    done = False
+    while not done:
+        ## Do Something here
+        #
+        #
+        #
+        #
+        #
+        #
+        #
+        #
+    return right
+
 def quicksort(myList, start, end):
     if start < end:
         # partition the list
-        pivot = #partition
+        pivot = partition(myList, start, end)
         # sort both halves
         quicksort(myList, start, pivot-1)
         quicksort(myList, pivot+1, end)
 
+def switch(myList, one, two):
+    temp=myList[one]
+    myList[one]=myList[two]
+    myList[two]=temp           
+
 def main():
-    myList = [3, 2, 4, 0, 7, 3, 5, 11]
-    sort(myList, 0, len(myList)-1)
+    myList = [3, 2, 4, 2, 7, 7, 5, 11]  
+    quicksort(myList, 0, len(myList)-1)
     print myList
 
-main() 
-Lock conversation
+main()
