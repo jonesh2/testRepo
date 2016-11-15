e77696e968af445a0224709d2956bf6ca1c1cea5
jonesh2 <jonesh2@carleton.edu>
2016-11-15 11:35:34 -0600

    finished implementing partition

--- sort.py
+++ sort.py
@@ -1,14 +1,38 @@
-def quicksort(myList, start, end):
+def sortMyList(myList):
+    newList = myList.copy()
+    quicksort(newList, 0, len(newList))
+    return newList
+
+def sort(myList, start, end):
     if start < end:
         # partition the list
-        pivot = partition(myList, start, end, start+1, end)
+        pivot = partition(myList, start, end)
         # sort both halves
         quicksort(myList, start, pivot-1)
         quicksort(myList, pivot+1, end)
 
+def partition(myList, start, end, left, right):
+    pivot = myList[start]
+    notDone = True
+    while notDone:
+        while left <= right and myList[left] <= pivot:
+            left = left + 1
+        while myList[right] >= pivot and right >=left:
+            right = right -1
+        if right < left:
+            done= True
+        else:
+            # swap places
+            swap(myList, left, right)
+    # swap start with myList[right]
+    swap(myList, start, right)
+    return right
+
+
+
 def main():
-    myList = [3, 2, 9, 0, 7, 3, 5, 12]
-    quicksort(myList, 0, len(myList)-1)
+    myList = [3, 2, 9, 0, 7, 3, 5, 11]
+    sort(myList, 0, len(myList)-1)
     print myList
 
 main()
