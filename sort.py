876f78cf153bce33ec4d4ca2b22783855d9bbd09
jonesh2 <jonesh2@carleton.edu>
2016-11-15 12:07:57 -0600

    base sort

---
+++ sort.py
@@ -0,0 +1,15 @@
+def quicksort(myList, start, end):
+    if start < end:
+        # partition the list
+        pivot = #partition
+        # sort both halves
+        quicksort(myList, start, pivot-1)
+        quicksort(myList, pivot+1, end)
+
+def main():
+    myList = [3, 2, 4, 0, 7, 3, 5, 11]
+    sort(myList, 0, len(myList)-1)
+    print myList
+
+main() 
+Lock conversation
